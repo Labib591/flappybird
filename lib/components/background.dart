656904c7395fabf4ed097aff71b9165eb0f1c09a
Mahir Mohammed Labib <mahi.labib5@gmail.com>
2024-11-08 14:08:31 +0600

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   lib/components/background.dart
    	modified:   lib/game.dart

--- lib/components/background.dart
+++ lib/components/background.dart
@@ -3,4 +3,7 @@ import 'package:flame/components.dart';
 class Background extends SpriteComponent{
   Background(Vector2 size) : super(size: size,position: Vector2(0,0));
   
+  onload() async{
+    sprite = await Sprite.load("background.png");
+  }
 }
--- lib/game.dart
+++ lib/game.dart
@@ -2,15 +2,21 @@ import 'dart:async';
 
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
+import 'package:flappybird/components/background.dart';
 import 'package:flappybird/components/bird.dart';
 
 class FlappyBirdGame extends FlameGame with TapDetector{
   late Bird bird;
+  late Background background;
 
   @override
   FutureOr<void> onLoad() {
     bird = Bird();
     add(bird);
+
+    background = Background(size);
+    add(background);
+
   }
 
   @override
